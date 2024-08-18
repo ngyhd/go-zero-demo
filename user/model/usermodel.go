3533300737aa3b5b87c107082c4e4faf8f40a70a
Sean <vip1-2-3-4-5@qq.com>
2024-08-19 00:03:39 +0800

    chore:初始化user服务

---
+++ user/model/usermodel.go
@@ -0,0 +1,29 @@
+package model
+
+import "github.com/zeromicro/go-zero/core/stores/sqlx"
+
+var _ UserModel = (*customUserModel)(nil)
+
+type (
+	// UserModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customUserModel.
+	UserModel interface {
+		userModel
+		withSession(session sqlx.Session) UserModel
+	}
+
+	customUserModel struct {
+		*defaultUserModel
+	}
+)
+
+// NewUserModel returns a model for the database table.
+func NewUserModel(conn sqlx.SqlConn) UserModel {
+	return &customUserModel{
+		defaultUserModel: newUserModel(conn),
+	}
+}
+
+func (m *customUserModel) withSession(session sqlx.Session) UserModel {
+	return NewUserModel(sqlx.NewSqlConnFromSession(session))
+}
---
+++ user/user.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/zeromicro/go-zero/core/logx"
+	"go-zero-demo/user/internal/config"
+	"go-zero-demo/user/internal/server"
+	"go-zero-demo/user/internal/svc"
+	"go-zero-demo/user/user"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var configFile = flag.String("f", "user/etc/user.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+	logx.DisableStat()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		user.RegisterUserServer(grpcServer, server.NewUserServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
